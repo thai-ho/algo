a0a6dcd35aed895a8ebca0ed975a8c83cdd34b41
thai-ho <hoangthai3107@gmail.com>
2025-06-15 23:59:18 +0700

    1432

--- leetcode/daily/1432-max-different-you-can-get-from-changing-an-interger/index.js
+++ leetcode/daily/1432-max-different-you-can-get-from-changing-an-interger/index.js
@@ -3,5 +3,43 @@
  * @return {number}
  */
 var maxDiff = function (num) {
-    
+  const numStr = num.toString();
+
+  function getMax(str) {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== "9") {
+        return parseInt(str.replaceAll(str[i], "9"));
+      }
+    }
+    return parseInt(str);
+  }
+
+  function getMin(str) {
+    if (str[0] !== "1") {
+      return parseInt(str.replaceAll(str[0], "1"));
+    }
+
+    for (let i = 1; i < str.length; i++) {
+      if (str[i] !== "0" && str[i] !== "1") {
+        return parseInt(str.replaceAll(str[i], "0"));
+      }
+    }
+
+    return parseInt(str);
+  }
+
+  const maxNum = getMax(numStr);
+  const minNum = getMin(numStr);
+
+  return maxNum - minNum;
 };
+
+const test = () => {
+  console.log("Test num=555:", maxDiff(555));
+  console.log("Test num=9:", maxDiff(9));
+  console.log("Test num=123:", maxDiff(123));
+  console.log("Test num=1111:", maxDiff(1111));
+  console.log("Test num=9000:", maxDiff(9000));
+};
+
+test();
